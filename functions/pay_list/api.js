2f321775249fa81ddf916cd097cde087d0a990c5
最好喝的小米粥 <admin@cunyx.cn>
2024-05-26 09:37:06 +0000

    赞助名单API获取
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- functions/pay_list/api.js
+++ functions/pay_list/api.js
@@ -1,1 +1,27 @@
 import xmz_ from '#xmz_';
+import axios from 'axios';
+import crypto from 'crypto';
+import querystring from 'querystring';
+async function api (page=1) { 
+  const api_key = xmz_.config('pay_list','afdian_key');
+  const api_user_id = xmz_.config('pay_list','afdian_uid');
+  const api_url = 'https://afdian.net/api/open/query-sponsor';
+  const time = Math.floor(Date.now() / 1000);
+  const params = { page };
+  const signData = `${api_key}params${JSON.stringify(params)}ts${time}user_id${api_user_id}`;
+  const sign = crypto.createHash('md5').update(signData).digest('hex');
+  const data = {
+    user_id: api_user_id,
+    ts: time,
+    sign: sign
+  };
+  const fullUrl = `${api_url}?${querystring.stringify({ params: JSON.stringify(params) })}&${querystring.stringify(data)}`;
+  axios.get(fullUrl)
+    .then(response => {
+      const apiReturn = response.data;
+      return apiReturn;
+    })
+    .catch(error => {
+      Bot.logger.err('获取数据失败', error);
+    });
+}
