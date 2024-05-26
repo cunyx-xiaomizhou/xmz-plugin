113d7d0f6b3c3196274ae7861adc87d03d9d3826
最好喝的小米粥 <admin@cunyx.cn>
2024-05-26 13:58:39 +0000

    调用一下导的函数
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/pay_list.js
+++ apps/pay_list.js
@@ -25,6 +25,10 @@ export class xmz_plugin_pay_list extends plugin {
       }
       return true;
     }
+    let page = e.msg.replace(/(赞助|投喂)(名单|列表)|#/gi,'');
+    if (page=='') {
+      page = 1;
+    }
     let afdian_key = xmz_.config(func,'afdian_key');
     let afdian_uid = xmz_.config(func,'afdian_uid');
     let be_paid_name = xmz_.config(func,'be_paid_name');
@@ -34,6 +38,20 @@ export class xmz_plugin_pay_list extends plugin {
     }
     if (be_paid_qq=='') {
       be_paid_qq = Bot.uin;
+    }
+    if (afdian_key==''||afdian_uif=='') {
+      e.reply('信息缺失，无法查询。请在小米粥插件下修改配置文件：\n'+xmz_.path+'/config/config');
+      return true;
+    }
+    try {
+      let apiReturn = xmz.pay_list.getApi(page);
+      if (!apiReturn||apiReturn[0]!=='{') {
+        e.reply('API返回出现错误，请稍后再试');
+      } else {
+        e.reply('测试消息：API请求成功，但真实性有待确认');
+      }
+    } catch (err) {
+      e.reply('API请求时出现错误：\n'+err,true);
     }
   }
 }
