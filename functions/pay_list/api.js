86d0553a512ea5e3c242b8039a76095f640127bb
最好喝的小米粥 <admin@cunyx.cn>
2024-05-27 14:02:49 +0000

    修改请求逻辑
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- functions/pay_list/api.js
+++ functions/pay_list/api.js
@@ -2,27 +2,28 @@ import xmz_ from '#xmz_';
 import axios from 'axios';
 import crypto from 'crypto';
 import querystring from 'querystring';
-async function api (page=1) { 
-  const api_key = await xmz_.config('pay_list','afdian_key');
-  const api_user_id = await xmz_.config('pay_list','afdian_uid');
-  const api_url = 'https://afdian.net/api/open/query-sponsor';
-  const time = Math.floor(Date.now() / 1000);
-  const params = { page:page };
-  const signData = `${api_key}params${JSON.stringify(params)}ts${time}user_id${api_user_id}`;
-  const sign = crypto.createHash('md5').update(signData).digest('hex');
-  const data = {
-    user_id: api_user_id,
-    ts: time,
-    sign: sign
-  };
-  const fullUrl = `${api_url}?${querystring.stringify({ params: JSON.stringify(params) })}&${querystring.stringify(data)}`;
-  axios.get(fullUrl)
-    .then(response => {
-      const apiReturn = response.data;
-      return apiReturn;
-    })
-    .catch(error => {
-      return error;
-    });
+
+async function api(page = 1) { 
+  try {
+    const func = 'pay_list';
+    const api_key = await xmz_.config(func, 'afdian_key');
+    const api_user_id = await xmz_.config(func, 'afdian_uid');
+    const api_url = 'https://afdian.net/api/open/query-sponsor';
+    const time = Math.floor(Date.now() / 1000);
+    const params = { page: page };
+    const signData = `${api_key}params${JSON.stringify(params)}ts${time}user_id${api_user_id}`;
+    const sign = crypto.createHash('md5').update(signData, 'utf8').digest('hex');
+    const data = {
+      user_id: api_user_id,
+      ts: time,
+      sign: sign
+    }
+    const fullUrl = `${api_url}?${querystring.stringify({ params: JSON.stringify(params) })}&${querystring.stringify(data)}`;
+    const response = await axios.get(fullUrl);
+    return JSON.stringify(response.data);
+  } catch (error) {
+    return error;
+  }
 }
+
 export { api };
