1b25b7a7169731664361d9307a33f9999263a963
最好喝的小米粥 <admin@cunyx.cn>
2024-06-01 08:33:27 +0000

    新的函数
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- functions/puppeteer/content/buffer.js
+++ functions/puppeteer/content/buffer.js
@@ -1,5 +1,13 @@
 import puppeteer from 'puppeteer';
 import { Buffer } from 'buffer';
+
+/**
+ * 生成网页截图并返回 Buffer
+ * @param {string} htmlString - 要渲染为截图的 HTML 字符串
+ * @param {number} viewportWidth - 视口宽度，默认为 1920
+ * @param {number} viewportHeight - 视口高度，默认为 1080
+ * @returns {Promise<Buffer|false>} - 返回生成的截图的 Buffer 或者 false 如果出现错误
+ */
 async function buffer(htmlString, viewportWidth = 1920, viewportHeight = 1080) {
   try {
     const browser = await puppeteer.launch({
@@ -11,12 +19,13 @@ async function buffer(htmlString, viewportWidth = 1920, viewportHeight = 1080) {
     const page = await browser.newPage();
     await page.setViewport({ width: viewportWidth, height: viewportHeight });
     await page.setContent(htmlString);
+    await page.waitForNavigation({ waitUntil: 'networkidle2' }); // 等待网络空闲状态
     const screenshotBuffer = await page.screenshot({ encoding: 'binary' });
     const bufferData = Buffer.from(screenshotBuffer, 'binary');
     await browser.close();
     return bufferData;
   } catch (err) {
-    throw err;
+    logger.error('图片生成失败：', err); // 使用 logger 函数记录错误
     return false;
   }
 }
