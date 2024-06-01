3b6284a125d0caf016a765d77d792281f1bd7484
最好喝的小米粥 <admin@cunyx.cn>
2024-06-01 07:10:46 +0000

    导出两种截图方法
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- functions/puppeteer/index.js
+++ functions/puppeteer/index.js
@@ -1,5 +1,9 @@
 import url from './url/index.js';
+import html from './html/index.js';
+import content from './content/index.js';
 let puppeteer = {
-    url: url
+    url: url,
+    html: html,
+    content: content
 };
 export default puppeteer;
