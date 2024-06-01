2c0797c9773b44ca8a20517a93674c504c82fa2a
最好喝的小米粥 <admin@cunyx.cn>
2024-06-01 02:50:54 +0000

    信息处理函数
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

---
+++ functions/pay_list/handle.js
@@ -0,0 +1,19 @@
+async function handle (jsonString) {
+  const Json = JSON.parse(jsonString);
+  let list = Json.data.list;
+  let userData = [];
+  for (const index = 0; index <= list.length; index++) {
+    let jsonData = list[index];
+    let money = jsonData.all_sum_amount;
+    let user = jsonData.user;
+    let userName = user.name;
+    let userAvatar = user.avatar;
+    let array = {
+      name: userName,
+      avatar: userAvatar
+    }
+    userDara.push(array);
+  }
+  return userData;
+}
+export { handle } ;
