b0d8cbf5de184da5ae6ba71be3a25f5b7446dd0a
最好喝的小米粥 <admin@cunyx.cn>
2024-06-01 08:31:42 +0000

    别等了
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- functions/puppeteer/content/buffer.js
+++ functions/puppeteer/content/buffer.js
@@ -11,7 +11,6 @@ async function buffer(htmlString, viewportWidth = 1920, viewportHeight = 1080) {
     const page = await browser.newPage();
     await page.setViewport({ width: viewportWidth, height: viewportHeight });
     await page.setContent(htmlString);
-    await page.waitForTimeout(2000); // 等待2秒加载图片或字体包等资源
     const screenshotBuffer = await page.screenshot({ encoding: 'binary' });
     const bufferData = Buffer.from(screenshotBuffer, 'binary');
     await browser.close();
