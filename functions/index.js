4ba1d5153a29b3d9a703b828eedf820249e677e4
最好喝的小米粥 <admin@cunyx.cn>
2024-06-02 03:01:52 +0000

    继续导出
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,14 @@
 import fs from 'fs';
 import tools from './tools.js';
 import { loading } from './loading.js';
+import { Copyright } from './Copyright.js';
 import pay_list from './pay_list/index.js'; 
 import puppeteer from './puppeteer/index.js';
 let xmz = {
     loading: loading,
     puppeteer: puppeteer,
     pay_list: pay_list,
-    tools: tools
+    tools: tools,
+    Copyright: Copyright
 };
 export default xmz;
