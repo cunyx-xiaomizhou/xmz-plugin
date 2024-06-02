5b2e40f56e64c3ff3f3eecc87d1e11721547eb4f
最好喝的小米粥 <admin@cunyx.cn>
2024-06-02 01:18:27 +0000

    更新工具函数
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- functions/tools.js
+++ functions/tools.js
@@ -1,15 +1,22 @@
 import xmz_ from '#xmz_';
 import fs from 'fs/promises';
-async function sent(Json) {
-    if (typeof Json === 'string') {
-        Json = JSON.parse(Json);
-    }
-    let json = JSON.stringify(Json, null, 2).replace(/\\\//g, '/');
-    return json;
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+async function sent(Json) {
+  if (typeof Json === 'string') {
+    Json = JSON.parse(Json);
+  }
+  let json = JSON.stringify(Json, null, 2).replace(/\\\//g, '/');
+  return json;
+}
+
 async function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
 async function mkdir(dirPath) {
   try {
     await fs.access(dirPath);
@@ -21,21 +28,25 @@ async function mkdir(dirPath) {
     }
   }
 }
+
 async function randomArray(array) {
-    let length = array.length;
-    let randomIndex = random(0,length - 1);
-    let randomElement = array[randomIndex];
-    return randomElement;
+  let length = array.length;
+  let randomIndex = random(0, length - 1);
+  let randomElement = array[randomIndex];
+  return randomElement;
 }
-async function getRes (functions, resPath) {
-  const data = await fs.readFile(xmz_.path+'/resource/'+functions+'/'+resPath, 'utf8');
+
+async function getRes(functions, resPath) {
+  const data = await fs.readFile(xmz_.path + '/resource/' + functions + '/' + resPath, 'utf8');
   return data;
 }
+
 let tools = {
-    sent: sent,
-    sleep: sleep,
-    mkdir: mkdir,
-    getRes: getRes,
-    randomArray: randomArray
+  sent: sent,
+  sleep: sleep,
+  mkdir: mkdir,
+  getRes: getRes,
+  randomArray: randomArray
 };
+
 export default tools;
