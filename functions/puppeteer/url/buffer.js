692e4635fc7b493414d7400d5ffe521baf72c2b4
最好喝的小米粥 <admin@cunyx.cn>
2024-06-02 06:49:51 +0000

    修改buffer函数
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- functions/puppeteer/url/buffer.js
+++ functions/puppeteer/url/buffer.js
@@ -1,21 +1,31 @@
 import puppeteer from 'puppeteer';
+import { Buffer } from 'buffer';
 async function buffer(url, viewportWidth = 1920, viewportHeight = 1080) {
+  let browser;
   try {
-    const browser = await puppeteer.launch({
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox'
-      ]
+    browser = await puppeteer.launch({
+      headless: "new",
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
     await page.setViewport({ width: viewportWidth, height: viewportHeight });
     await page.goto(url, { waitUntil: 'networkidle0' });
-    const buffer = await page.screenshot({ fullPage: true, encoding: 'binary' });
+    await page.evaluateHandle('document.fonts.ready');
+    const bodyHandle = await page.$('body');
+    const { height } = await bodyHandle.boundingBox();
+    await page.setViewport({ width: viewportWidth, height: Math.ceil(height) });
+    await page.waitForTimeout(5000);
+    const screenshotBuffer = await page.screenshot({ encoding: 'binary', fullPage: true });
+    const bufferData = Buffer.from(screenshotBuffer, 'binary');
     await browser.close();
-    return buffer;
+    return bufferData;
   } catch (err) {
-    Bot.logger.err('截图失败：', err);
-    throw err;
+    logger.error('图片生成失败：', err); 
+    if (browser) {
+      await browser.close();
+    }
+    return false;
   }
 }
+
 export { buffer };
