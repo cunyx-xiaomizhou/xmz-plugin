5b990dd79e3aaaac4d3665d5845f0c5c6dbdd8c8
最好喝的小米粥 <admin@cunyx.cn>
2024-06-02 01:23:48 +0000

    更新截图函数
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- functions/puppeteer/content/buffer.js
+++ functions/puppeteer/content/buffer.js
@@ -3,31 +3,42 @@ import { Buffer } from 'buffer';
 
 /**
  * 生成网页截图并返回 Buffer
- * @param {string} htmlString - 要渲染为截图的 HTML 字符串
- * @param {number} viewportWidth - 视口宽度，默认为 1920
- * @param {number} viewportHeight - 视口高度，默认为 1080
- * @returns {Promise<Buffer|false>} - 返回生成的截图的 Buffer 或者 false 如果出现错误
+ * @param {string} htmlString - 要渲染为截图的 HTML 字符串.
+ * @param {number} viewportWidth - 视口宽度，默认为 1920.
+ * @param {number} viewportHeight - 视口高度，默认为 1080.
+ * @returns {Promise<Buffer|false>} - 返回生成的截图的 Buffer 或者 false 如果出现错误.
  */
+
 async function buffer(htmlString, viewportWidth = 1920, viewportHeight = 1080) {
+  let browser;
   try {
-    const browser = await puppeteer.launch({
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox'
-      ]
+    browser = await puppeteer.launch({
+      headless: "new", // 使用新的Headless模式.
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
     await page.setViewport({ width: viewportWidth, height: viewportHeight });
-    await page.setContent(htmlString);
-    await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 20000 }); // 等待网络空闲状态
-    const screenshotBuffer = await page.screenshot({ encoding: 'binary' });
+    await page.setContent(htmlString, { waitUntil: 'domcontentloaded' });
+    // 等待页面完全加载.
+    await page.evaluateHandle('document.fonts.ready');
+    // 自动调整页面高度.
+    const bodyHandle = await page.$('body');
+    const { height } = await bodyHandle.boundingBox();
+    await page.setViewport({ width: viewportWidth, height: Math.ceil(height) });
+    // 延时等待图片等资源加载完成.
+    await page.waitForTimeout(5000);
+    const screenshotBuffer = await page.screenshot({ encoding: 'binary', fullPage: true });
     const bufferData = Buffer.from(screenshotBuffer, 'binary');
     await browser.close();
     return bufferData;
   } catch (err) {
-    logger.error('图片生成失败：', err); // 使用 logger 函数记录错误
+    logger.error('图片生成失败：', err); // 使用logger函数记录错误.
+    if (browser) {
+      await browser.close();
+    }
     return false;
   }
 }
 
 export { buffer };
+
