2bf890342bfd8d0249fcd4e15690e49424189a6b
最好喝的小米粥 <admin@cunyx.cn>
2024-06-10 08:45:55 +0000

    写一点，不写太多
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/banTurntable.js
+++ apps/banTurntable.js
@@ -1,3 +1,4 @@
+import fs from 'fs/promises';
 import xmz from '#xmz';
 import xmz_ from '#xmz_';
 import plugin from './../../../lib/plugins/plugin.js';
@@ -20,11 +21,28 @@ export class banTurntable_xmz_plugin extends plugin {
     });
   }
   async switches(e) {
+    if (!e.group_id) {
+      e.reply('❌ 此功能仅能在群聊里使用',true);
+      return false;
+    }
+    let switches = e.msg.replace(/禁言大转盘|#/gi,'');
+    let condition = switches.includes('开启') ? true : false;
     let state = await xmz_.comfig(func,'state');
     if (state) {
       const dataPath = `${xmz_.path}/data/${func}`;
       const filePath = `${dataPath}/${e.group_id}.json`;
-      await xmz.tools.mkdir(dataPath)
+      await xmz.tools.mkdir(dataPath);
+      try {
+        let Data = JSON.parse(await fs.readFile(filePath));
+      } catch (err) {
+        let Data = {
+            state: condition,
+            cd: {
+              group: Date.now(),
+              member: {}
+            }
+        };
+      }
     } else {
       e.reply(`此功能未开启，请联系机器人主人在${xmz_.path}/config/config/${func}.json中开启`,true);
     }
