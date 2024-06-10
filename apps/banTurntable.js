8589ebdb88d772e02ff9384736f41b61e7f7953e
最好喝的小米粥 <admin@cunyx.cn>
2024-06-10 06:08:06 +0000

    禁言大转盘游戏框架（暂无法使用）
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/banTurntable.js
+++ apps/banTurntable.js
@@ -1,1 +1,26 @@
-//禁言大转盘，先写配置文件
+import xmz from '#xmz';
+import xmz_ from '#xmz_';
+import plugin from './../../../lib/plugins/plugin.js';
+
+const func = 'banTurntable';
+
+export class banTurntable_xmz_plugin extends plugin {
+  constructor () {
+    super({
+      name:"禁言大转盘",
+      dsc:"我就喜欢被禁言的感觉",
+      event:"message",
+      priority:1,/*优先级*/
+      rule:[
+        {
+          reg:"^#?(开启|关闭)禁言大转盘",
+          fnc:"switches"
+        }
+      ]
+    });
+  }
+  async switches(e) {
+    const dataPath = `${xmz_.path}/data/${func}`;
+    await xmz.tools.mkdir(dataPath);
+  }
+}
