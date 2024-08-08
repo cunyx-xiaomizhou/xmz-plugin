a7e4ea0b8003fae01b14c712a3a863e28971ca04
最好喝的小米粥 <admin@cunyx.cn>
2024-08-08 01:47:59 +0000

    适配单群自定义
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
-async function config(fileName, key) {
+async function config(fileName, key, Key='default') {
   const config_path = process.cwd() + '/plugins/xmz-plugin/config/config/';
   let filePath = config_path + fileName + '.json';
   let content = await fs.readFileSync(filePath);
   let Json = await JSON.parse(content);
-  return Json.config[key];
+  return (Key=='default') ? Json.config[key] : Json.config[Key][key];
 }
 export { config };
