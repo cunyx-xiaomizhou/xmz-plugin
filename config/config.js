3a3a9f6f67a41461c284c7a9a17be1a416130c7e
最好喝的小米粥 <admin@cunyx.cn>
2024-08-09 11:58:48 +0000

    再次修改获取逻辑
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
 import fs from 'fs';
+
 async function config(fileName, key, Key) {
   const config_path = process.cwd() + '/plugins/xmz-plugin/config/config/';
   let filePath = config_path + fileName + '.json';
   let content = await fs.readFileSync(filePath);
   let Json = await JSON.parse(content);
-  return (Json.config[Key][key]) ? Json.config[Key][key] : Json.config[key];
+  
+  return (Key && Key in Json.config && key in Json.config[Key]) ? Json.config[Key][key] : Json.config[key];
 }
+
 export { config };
