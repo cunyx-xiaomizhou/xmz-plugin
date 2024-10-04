8a5b5fb6f9d88ca3266209e467e1ebd44a093d93
最好喝的小米粥 <admin@cunyx.cn>
2024-10-04 08:10:10 +0000

    🍵 喝口茶缓缓
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/ban.js
+++ apps/ban.js
@@ -98,8 +98,8 @@ export class xmz_ban extends plugin {
     let ratio_ban = await xmz_.config(func, 'ratio_ban', e.group_id);
     try {
       if (
-          Bot.pickGroup(e.group_id,true).pickMember(qq,true).is_admin ||
-          Bot.pickGroup(e.group_id,true).pickMember(qq,true).is_owner
+          Bot.pickGroup(e.group_id).pickMember(qq).is_admin ||
+          Bot.pickGroup(e.group_id).pickMember(qq).is_owner
         ) {
         if (e.member.is_admin || e.member.is_owner) {
           e.reply('❌ 管理之间至于这么狠嘛😳....',true);
