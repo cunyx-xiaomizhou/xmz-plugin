508fb928425a6229dd18361fecdaa048737f6304
最好喝的小米粥 <admin@cunyx.cn>
2024-10-04 02:12:02 +0000

    📲 载入插件框架
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/ban.js
+++ apps/ban.js
@@ -1,2 +1,21 @@
-// 口球玩法文件
-// 开始策划玩法
+import plugin from './../../../lib/plugins/plugin.js';
+
+export class xmz_ban extends plugin {
+  constructor () {
+    super({
+      name:"口球禁言",
+      dsc:"使用米粥币禁言群员",
+      event:"message",
+      priority:1,/*优先级*/
+      rule:[
+        {
+            reg:"^#?口球(.*)?",
+            fnc:"ban",
+        }
+      ]
+    });
+  }
+  async ban(e) {
+    e.reply('正则触发成功！');
+  }
+}
