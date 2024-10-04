8957efc9fba9faa7463dfa8c256c6268eb0dfbfe
最好喝的小米粥 <admin@cunyx.cn>
2024-10-04 02:17:25 +0000

    🥥 获取QQ号逻辑更新
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/ban.js
+++ apps/ban.js
@@ -16,6 +16,23 @@ export class xmz_ban extends plugin {
     });
   }
   async ban(e) {
-    e.reply('正则触发成功！');
+    if (e.group_id) {
+      e.reply('❌ 本功能仅能在群聊使用',true);
+      return false;
+    }
+    if (!(e.group.is_admin || e.group.is_owner)) {
+      e.reply('❌ 机器人非管理员/群主，无法使用此功能');
+      return true;
+    }
+    let qq;
+    qq = e.msg.replace(/禁言大转盘|#/gi, '').trim();
+    if (qq=='') {
+      qq = e.message.filter(item => item.type == 'at')?.map(item => item?.qq);
+    }
+    if (qq=='') {
+      qq = e.user_id;
+      await xmz.tools.sleep(2000);
+    }
+    e.reply(`✅ 被操作者QQ号：${qq}`,true);
   }
 }
