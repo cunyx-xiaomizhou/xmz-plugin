5d1b89152703b9d16055b06b46d0740b9bcca681
最好喝的小米粥 <admin@cunyx.cn>
2024-10-05 02:32:41 +0000

    🍝 后门开关
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/setting.js
+++ apps/setting.js
@@ -10,9 +10,23 @@ export class xmz_plugin_setting extends plugin {
       rule:[
         {
           reg:"^#?小米粥设置后门(开启|关闭)",
-          fnc:"xmz"
+          fnc:"izhou"
         }
       ]
     });
+  }
+  async izhou(e) {
+    try {
+      const state = await xmz.xmz(e);
+      if (!state[0]) {
+        e.reply(`❌ ${state[1]}`,true);
+        return true;
+      }
+      let newValue = e.msg.includes(/开启/gi) ? true : false;
+      await xmz.tools.uc('xmz', 'state', newValue);
+    } catch(err) {
+      e.reply(`❌ 运行时出现错误：\n\n${err}`,true);
+      return false;
+    }
   }
 }
