14c76d8acfa30e6c2617327162c2028842f6b593
最好喝的小米粥 <admin@cunyx.cn>
2024-12-09 17:00:19 +0000

    🍜 打完收工
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/video.js
+++ apps/video.js
@@ -18,6 +18,39 @@ export class xmz_plugin_video extends plugin {
     });
   }
   async video(e) {
-    xmz.tools.sendMsg(e, ['你好','我不好'])
+    const uid = await xmz_.config('xmzTools', 'uid');
+    const api_key = await xmz_.config('xmzTools', 'api_key');
+    if (!uid||!api_key||uid==''||api_key=='') {
+      if (await xmz_.config('video', 'reply')) {
+        e.reply('❌ 解析失败，缺少小米粥工具箱uid或api_key',true);
+        return false;
+      }
+    }
+    const urls = await xmz.tools.exu(e.msg);
+    let video_url = urls[0];
+    const api_url = 'https://tools.xmz.netkj.com/api/API/video_url';
+    let json;
+    try {
+      json = JSON.parse(await fetch(`${api_url}?uid=${uid}&api_key=${api_key}&url=${url}`));
+      if (json.code == 200) {
+        if (json.data.type == 1) {
+          sendMsg(e, [`视频标题：${json.data.title}`, '视频封面', json.data.cover_url]);
+          e.reply(segment.video(json.data.video_url));
+        } else {
+          let arr_index = ['图集标题：'+json.data.title, '图集封面：', json.data.cover_url];
+          let send_arr = [...arr_index,...json.data.pics];
+          sendMsg(e, send_arr);
+        }
+      } else {
+        if (await xmz_.config('video', 'replyFail')) {
+          e.reply(`❌${json.msg}`,true);
+        }
+      }
+    } catch (err) {
+      if (await xmz_.config('video', 'replyFail')) {
+        e.repky(`❌ 出了点小问题：\n\n${err}`,true);
+      }
+    }
   }
+  return false;
 }
