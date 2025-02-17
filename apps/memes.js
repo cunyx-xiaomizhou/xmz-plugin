5a5713ffb634bcd9c891b8560419716eba29335c
最好喝的小米粥 <admin@cunyx.cn>
2025-02-17 06:11:52 +0000

    😅😅😅 meme表情包
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

---
+++ apps/memes.js
@@ -0,0 +1,38 @@
+import xmz from '#xmz';
+import xmz_ from '#xmz_';
+const meme_api = 'https://tools.xmz.netkj.com/api/API/memes/'; 
+export class xmz_plugin_memes extends plugin {
+  constructor () {
+    super({
+      name:"表情包生成",
+      dsc:"小米粥工具箱独家表情包内容",
+      event:"message",
+      priority:10086,
+      rule:[
+        {
+          reg:/#?(wz(ry)?|5v5)(.*)?/gi,
+          fnc:'王者荣耀'
+        }
+      ]
+    });
+  }
+  async 王者荣耀(e) {
+    const uid = await xmz_.config('xmzTools', 'uid');
+    const api_key = await xmz_.config('xmzTools', 'api_key');
+    if (!uid||!api_key||uid==''||api_key=='') {
+      e.reply('❌ 生成失败，还未设置小米粥工具箱UID和API_key')
+    }
+    let json;
+    try {
+      json = await (await fetch(`${meme_api}5v5?uid=${uid}&api_key=${api_key}&url=${url}`)).json();
+      if (json.code == 200) {
+        e.reply(segment.image('base64://'+json.data.base64));
+      } else {
+        e.reply(`❌${json.msg}`,true);
+      }
+    } catch (err) {
+      e.reply(`❌ 出了点小问题：\n\n${err}`,true);
+    }
+    return false;
+  }
+}
